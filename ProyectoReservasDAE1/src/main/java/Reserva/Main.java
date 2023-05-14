9eb6df2baf4bdd87801e94f76db1b9dafe498790
Ricardo Alcantara <alcantararicardo04@gmail.com>
2023-05-13 21:49:16 -0500

    Agregando proyecto y driver mysql

---
+++ ProyectoReservasDAE1/src/main/java/Reserva/Main.java
@@ -0,0 +1,15 @@
+package Reserva;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String database = "reservas";
+        System.out.println("Oh baby i miss you");
+        String url = "jdbc:mysql://localhost:3306/myDb user1 pass";
+        Connection con = DriverManager.getConnection(url);
+    }
+}
