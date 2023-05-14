387ffc287086c4a84b8fbf119ef1be2aec8c11d3
f3rc1t0 <132720607+f3rc1t0@users.noreply.github.com>
2023-05-13 23:35:32 -0500

    Creación de Clase Recepcionista, métodos Set/Get/registrarReserva/cancelarReserva

--- ProyectoReservasDAE1/src/main/java/Reserva/Main.java
+++ ProyectoReservasDAE1/src/main/java/Reserva/Main.java
@@ -11,5 +11,8 @@ public class Main {
         System.out.println("Oh baby i miss you");
         String url = "jdbc:mysql://localhost:3306/myDb user1 pass";
         Connection con = DriverManager.getConnection(url);
+
+        Recepcionista recepcionista1 = new Recepcionista("Juan","Perez","JP");
+        System.out.println(recepcionista1.getNombre());
     }
 }
