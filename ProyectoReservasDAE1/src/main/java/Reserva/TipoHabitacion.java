88e21979e06b864228d9e5b0f86762af452b79fb
Franco1097 <gian.franco.lozano@hotmail.com>
2023-05-14 00:23:43 -0500

    Creacion de clase TipoHabitacion

---
+++ ProyectoReservasDAE1/src/main/java/Reserva/TipoHabitacion.java
@@ -0,0 +1,38 @@
+package Reserva;
+
+public class TipoHabitacion {
+
+    int codigoHabitacion;
+    String descripcion;
+    double tarifa;
+
+    public TipoHabitacion(int codigoHabitacion, String descripcion, double tarifa) {
+        this.codigoHabitacion = codigoHabitacion;
+        this.descripcion = descripcion;
+        this.tarifa = tarifa;
+    }
+
+    public int getCodigoHabitacion() {
+        return codigoHabitacion;
+    }
+
+    public void setCodigoHabitacion(int codigoHabitacion) {
+        this.codigoHabitacion = codigoHabitacion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getTarifa() {
+        return tarifa;
+    }
+
+    public void setTarifa(double tarifa) {
+        this.tarifa = tarifa;
+    }
+}
