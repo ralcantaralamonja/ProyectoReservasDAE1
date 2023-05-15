8a7408ee7a79359eea5b53a716afeface493e1dd
Ricardo Alcantara <alcantararicardo04@gmail.com>
2023-05-14 23:02:59 -0500

    Corrigiendo clase Hotel

--- ProyectoReservasDAE1/src/main/java/Reserva/Hotel.java
+++ ProyectoReservasDAE1/src/main/java/Reserva/Hotel.java
@@ -4,33 +4,32 @@ import java.util.Date;
 
 public class Hotel {
 
-String Cod_Hot = "";
-String Dir_Hc = "";
+String Cod_Ho = "";
+String Dir_Ho = "";
 String Tel_Hc = "";
+Date Fec_Const;
 
-    public Hotel(String cod_Hot, String dir_Hc, String tel_Hc, Date fec_Const) {
-        Cod_Hot = cod_Hot;
-        Dir_Hc = dir_Hc;
+    public Hotel(String cod_Ho, String dir_Ho, String tel_Hc, Date fec_Const) {
+        Cod_Ho = cod_Ho;
+        Dir_Ho = dir_Ho;
         Tel_Hc = tel_Hc;
         Fec_Const = fec_Const;
     }
 
-    public String getCod_Hot() {
-        return Cod_Hot;
+    public String getCod_Ho() {
+        return Cod_Ho;
     }
 
-    public void setCod_Hot(String cod_Hot) {
-        Cod_Hot = cod_Hot;
+    public void setCod_Ho(String cod_Ho) {
+        Cod_Ho = cod_Ho;
     }
 
-    Date Fec_Const ;
-
-    public String getDir_Hc() {
-        return Dir_Hc;
+    public String getDir_Ho() {
+        return Dir_Ho;
     }
 
-    public void setDir_Hc(String dir_Hc) {
-        Dir_Hc = dir_Hc;
+    public void setDir_Ho(String dir_Ho) {
+        Dir_Ho = dir_Ho;
     }
 
     public String getTel_Hc() {
@@ -48,6 +47,4 @@ String Tel_Hc = "";
     public void setFec_Const(Date fec_Const) {
         Fec_Const = fec_Const;
     }
-
-
 }
