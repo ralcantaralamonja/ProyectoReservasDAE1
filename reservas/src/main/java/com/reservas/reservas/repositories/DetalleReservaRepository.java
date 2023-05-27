eb99364005baab6976bdec601317beccf8daf8f6
Ricardo Alcantara <alcantararicardo04@gmail.com>
2023-05-26 23:24:05 -0500

    Actualizando Detalle

--- reservas/src/main/java/com/reservas/reservas/repositories/DetalleReservaRepository.java
+++ reservas/src/main/java/com/reservas/reservas/repositories/DetalleReservaRepository.java
@@ -1,7 +1,10 @@
 package com.reservas.reservas.repositories;
 
+import com.reservas.reservas.entidades.Cliente;
+import com.reservas.reservas.entidades.DetalleReserva;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DetalleReservaRepository {
+public interface DetalleReservaRepository extends JpaRepository<DetalleReserva, Long> {
 }
